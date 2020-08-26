7ab80007f40be8a86acbd1ae3b2f5692611f2862
suganya somu <sweetsugan12@gmail.com>
2020-08-25 21:32:33 -0700

    working code

--- script.js
+++ script.js
@@ -1,78 +1,197 @@
 $(document).ready(function () {
-    var enterInput = $("#enterInput");
+    var enterPositionInput = $("#enterInput");
+    //var enterLocationInput = $("#locationInput")
     var text1 = $("#text1");
     var text2 = $("#text2");
     var text3 = $("#text3");
-    var location = $("#location");
-    var organization = $("#organization");
-    console.log(enterInput);
+    var col = $("#side-col");
 
-    var position = "Software Developer/Engineer";
+    //var position = "Software Developer/Engineer";
+    var location = "";
+    var postion = "";
     //Working API
-
-
-    var query = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=120&t.k=fz6JLNDfgVs&action=employers&q=pharmaceuticals&userip=192.168.43.42&useragent=Mozilla/%2F4.0"
-
-    //var query = "https://www.reed.co.uk/api/1.0/search?keywords=accountant&location=london&employerid=123&distancefromlocation=15";
-
-    var query2 = "https://jooble.org/api/28b271c0-9010-40fe-aca1-76270d4e740e"
-
-
     function search() {
-
-        var position = enterInput.val();
+        var position = enterPositionInput.val();
+        //var location = enterLocationInput.val();
+        //    / if (enterLocationInput == "" || enterPositionInput == "") {
+        //         return false;
+        //     };
         console.log(position);
+        console.log(location);
 
-        var queryURL = "https://data.usajobs.gov/api/search?PositionTitle=" + position + "&appid=/hRFG+7lMOGKbS2O8/v+5w49kiotWe8/WYqU8XKqBRA="
+        var queryURL =
+            "https://data.usajobs.gov/api/search?PositionTitle=" +
+            position +
+            "&location= " +
+            location +
+            "&appid=/hRFG+7lMOGKbS2O8/v+5w49kiotWe8/WYqU8XKqBRA=";
         $.ajax({
             url: queryURL,
             method: "GET",
             headers: {
-                'Authorization-Key': '/hRFG+7lMOGKbS2O8/v+5w49kiotWe8/WYqU8XKqBRA='
+                "Authorization-Key": "/hRFG+7lMOGKbS2O8/v+5w49kiotWe8/WYqU8XKqBRA=",
             },
-
         }).then(function (response) {
             console.log(response);
 
-            var positionTitle = response.SearchResult.SearchResultItems[0].MatchedObjectDescriptor.PositionTitle;
-            var LocationName = response.SearchResult.SearchResultItems[0].MatchedObjectDescriptor.PositionLocation[0].LocationName;
-            var organizationName = response.SearchResult.SearchResultItems[0].MatchedObjectDescriptor.OrganizationName;
+            for (var i = 0; i < response.SearchResult.SearchResultItems.length; i++) {
 
-            var pTag1 = $("<p>");
-            var pTag2 = $("<p>");
+                // if (response.SearchResult.SearchResultItems.length < 5) {
+                var positionTitle =
+                    response.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
+                    .PositionTitle;
 
-            text1.append(pTag1);
-            text1.append(pTag2);
+                var LocationName = "oregon";
+                // response.SearchResult.SearchResultItems[i].MatchedObjectDescriptor[i]
+                //     .PositionLocation[0].LocationName;
+                var organizationName =
+                    response.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
+                    .OrganizationName;
 
-            console.log(pTag1.text("Position Title: " + positionTitle));
-            console.log(pTag2.text("Location Name: " + LocationName));
-            // location.text(LocationName);
-            // organization.text(organizationName);
+                var divCard = $("<div>");
+                divCard.addClass("column");
+                // divCard.css("display", "block");
+                // divCard.css("width", "block");
+                var divContent = $("<div>");
+                divContent.addClass("card-content");
+
+                var p1 = $("<p>").text("position: " + positionTitle);
+                p1.addClass("card-header-title");
+                p1.css("font", "100");
+
+
+                var p2 = $("<p>").text("Location: " + LocationName);
+                p2.addClass("card-header-title");
+                p2.css("font", "100");
+                var p3 = $("<p>").text("Organization: " + organizationName);
+                p3.addClass("card-header-title");
+                p3.css("font", "100");
+
+                divCard.append(divContent);
+                divContent.append(p1);
+
+                divContent.append(p2);
+                divContent.append(p3);
+
+                col.append(divCard);
+            }
+
+            // }
 
 
 
-        })
+        });
     }
 
-    /* function search2() {
-            var query2 = "http://api.jobs2careers.com/api/search.php?id=273&pass=HkdyhY4qQUmJXi5p&ip=...&q=...&l="
+    /*function search2() {
+           var jobSearch = {
+               "async": true,
+               "crossDomain": true,
+               "url": "https://indeed-com.p.rapidapi.com/search/jobs?sort=relevance&location="+ searchLocation +"&offset=0&query="+ position +"&country=us&radius=25",
+                    divContent.append(p3);
 
-            $.ajax({
-                url: query2,
-                method: "GET",
-                data: "JSON"
-                
+                    col.append(divCard);
+                }
 
-            }).then(function (response) {
-                console.log(response);
-            });
+            }
 
-        }
-    */
 
+
+        });
+    }
+
+    /*function search2() {
+           var jobSearch = {
+               "async": true,
+               "crossDomain": true,
+               "url": "https://indeed-com.p.rapidapi.com/search/jobs?sort=relevance&location="+ searchLocation +"&offset=0&query="+ position +"&country=us&radius=25",
+                    divContent.append(p3);
+
+                    col.append(divCard);
+                }
+
+            }
+
+
+
+        });
+    }
+
+    /*function search2() {
+           var jobSearch = {
+               "async": true,
+               "crossDomain": true,
+               "url": "https://indeed-com.p.rapidapi.com/search/jobs?sort=relevance&location="+ searchLocation +"&offset=0&query="+ position +"&country=us&radius=25",
+                    divContent.append(p3);
+
+                    col.append(divCard);
+                }
+
+            }
+
+
+
+        });
+    }
+
+    /*function search2() {
+           var jobSearch = {
+               "async": true,
+               "crossDomain": true,
+               "url": "https://indeed-com.p.rapidapi.com/search/jobs?sort=relevance&location="+ searchLocation +"&offset=0&query="+ position +"&country=us&radius=25",
+                    divContent.append(p3);
+
+                    col.append(divCard);
+                }
+
+            }
+
+
+
+        });
+    }
+
+    /*function search2() {
+           var jobSearch = {
+               "async": true,
+               "crossDomain": true,
+               "url": "https://indeed-com.p.rapidapi.com/search/jobs?sort=relevance&location="+ searchLocation +"&offset=0&query="+ position +"&country=us&radius=25",
+                    divContent.append(p3);
+
+                    col.append(divCard);
+                }
+
+            }
+
+
+
+        });
+    }
+
+    /*function search2() {
+           var jobSearch = {
+               "async": true,
+               "crossDomain": true,
+               "url": "https://indeed-com.p.rapidapi.com/search/jobs?sort=relevance&location="+ searchLocation +"&offset=0&query="+ position +"&country=us&radius=25",
+               "method": "GET",
+               "headers": {
+                   "x-rapidapi-host": "indeed-com.p.rapidapi.com",
+                   "x-rapidapi-key": "edeaaf9e95msh0a5c23597d14061p1bea7fjsn33851cc7a8f1"
+               }
+           }
+
+       $.ajax(jobSearch).then(function (response1) {
+           console.log(response1);
+           var positionTitle2 = response1.results[i].jobtitle;
+           var locationName2 = response1.results[i].formattedLocation;
+           var organizationName2 = response1.results[i].company;
+           
+       });
+       
+       };
+       */
 
 
     $("#submit").on("click", search);
     // search2();
-
 });
