eea33d5d742f34b9a536f24b01a8d154288d5418
suganya somu <sweetsugan12@gmail.com>
2020-08-27 18:53:10 -0700

    made changes to the design

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ $(document).ready(function () {
         console.log(location);
 
         var queryURL =
-            "https://data.usajobs.gov/api/search?PositionTitle=" +
+            "https://data.usajobs.gov/api/search?Keyword=" +
             position +
-            "&location= " +
+            "&locationName= " +
             location +
             "&appid=/hRFG+7lMOGKbS2O8/v+5w49kiotWe8/WYqU8XKqBRA=";
         $.ajax({
@@ -37,11 +37,7 @@ $(document).ready(function () {
         }).then(function (response) {
             console.log(response);
 
-
-
             for (var i = 0; i < response.SearchResult.SearchResultItems.length; i++) {
-
-
                 var positionTitle =
                     response.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
                     .PositionTitle;
@@ -53,9 +49,9 @@ $(document).ready(function () {
                     response.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
                     .OrganizationName;
 
-
                 var mainDiv = $("<div>");
                 mainDiv.addClass("columns is-vcentered");
+                mainDiv.css("border-radius", "10px");
 
                 var divCard = $("<div>");
                 divCard.addClass("column is-3");
@@ -65,12 +61,9 @@ $(document).ready(function () {
                 var divContent = $("<div>");
                 divContent.addClass("card-content");
 
-
-
                 var p1 = $("<p>").text("Position: " + positionTitle);
                 p1.addClass("card-header-title");
                 p1.css("font", "100");
-
 
                 var p2 = $("<p>").text("Location: " + LocationName);
                 p2.addClass("card-header-title");
@@ -80,10 +73,18 @@ $(document).ready(function () {
                 p3.css("font", "100");
                 var p4 = $("<p>");
                 p4.addClass("card-header-title");
-                p4.attr("data-value", response.SearchResult.SearchResultItems[i].MatchedObjectDescriptor.PositionURI);
+                p4.attr(
+                    "data-value",
+                    response.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
+                    .PositionURI
+                );
                 var p5 = $("<p>");
                 p5.addClass("card-header-title");
-                p5.attr("data-qualify", response.SearchResult.SearchResultItems[i].MatchedObjectDescriptor.QualificationSummary);
+                p5.attr(
+                    "data-qualify",
+                    response.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
+                    .QualificationSummary
+                );
 
                 divCard.append(divContent);
                 divContent.append(p1);
@@ -100,8 +101,7 @@ $(document).ready(function () {
                 mainDiv.css("width", "30%");
                 mainDiv.css("border", "3px solid purple");
                 divCard.css("background-color", "#ffffff");
-                divCard.css("width", "100%")
-
+                divCard.css("width", "100%");
             }
 
             var position =
@@ -114,42 +114,32 @@ $(document).ready(function () {
 
             link.css("display", "inline-block");
             qualification.css("display", "inline-block");
-            var summary = response.SearchResult.SearchResultItems[0].MatchedObjectDescriptor
+            var summary =
+                response.SearchResult.SearchResultItems[0].MatchedObjectDescriptor
                 .QualificationSummary;
 
             info1.text(position);
             info2.text(location);
             details.text(summary);
-
-
-
         });
     }
 
-
-
     function displayDetails() {
-
-        $(".is-3").css("border-left", "0px")
-            .css("height", "300px");
-        var text1 = $(this).find('p:nth-child(1)').text();
-        var text2 = $(this).find('p:nth-child(2)').text();
-        text4 = $(this).find('p:nth-child(4)').attr("data-value");
-        text5 = $(this).find('p:nth-child(5)').attr("data-qualify");
-        $(info1).innerHTML = '';
-        $(info2).innerHTML = '';
+        $(".is-3").css("border-left", "0px").css("height", "300px");
+        var text1 = $(this).find("p:nth-child(1)").text();
+        var text2 = $(this).find("p:nth-child(2)").text();
+        text4 = $(this).find("p:nth-child(4)").attr("data-value");
+        text5 = $(this).find("p:nth-child(5)").attr("data-qualify");
+        $(info1).innerHTML = "";
+        $(info2).innerHTML = "";
         $(info1).text(text1);
         $(info2).text(text2);
         $(details).text(text5);
-        $(this).css("border-left", "6px solid green")
-            .css("height", "300px");
-
+        $(this).css("border-left", "6px solid green").css("height", "300px");
     }
     $("#submit").on("click", search);
     $(document).on("click", ".is-3", displayDetails);
     $("#link").click(function () {
         window.open(text4);
     });
-
-
 });
